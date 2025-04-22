d444bfeeef2cab4a8f50e256deb141f07d505d3e
VuHuyDat <156570762+VuHuyDat@users.noreply.github.com>
2025-04-22 15:02:40 +0700

    Update Vu Huy Dat_2321060062.py

--- Vu Huy Dat_2321060062.py
+++ Vu Huy Dat_2321060062.py
@@ -37,6 +37,14 @@ else:
     st.write("Biểu đồ số lượng phim theo thể loại:")
     plt.figure(figsize=(10, 6))
     plt.bar(genre_counts.index, genre_counts, color='lightgreen', edgecolor='black')
+
+    st.subheader("Điểm đánh giá trung bình theo năm")
+    avg_score_by_year = data.groupby('year')['score'].mean().reset_index()
+
+    fig1, ax1 = plt.subplots(figsize=(12, 5))
+    plt.lineplot(data=avg_score_by_year, x='year', y='score', ax=ax1, marker='o', color='orange')
+    ax1.set_title("Score trung bình theo năm")
+    st.pyplot(fig1)
     
     plt.xlabel('Thể loại (Genre)')
     plt.ylabel('Số lượng phim')
